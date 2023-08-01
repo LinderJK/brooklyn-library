b6ffd8b1d5f403b89813956ac77b0fccd74d0e82
ilya slepchenkov <slepchenkovia@gmail.com>
2023-08-01 23:25:20 +0300

    feat: add burger menu

---
+++ library/js/script.js
@@ -0,0 +1,18 @@
+const navbarTogglerButton = document.querySelector('.navbar-toggler');    
+const navListToggler = document.querySelector('.nav__list');
+
+navbarTogglerButton.addEventListener('click', function () {
+        navbarTogglerButton.classList.toggle('navbar-toggler--active');
+        navListToggler.classList.toggle('nav__list--active');
+    })
+
+document.addEventListener ('click', function(event) {
+    if(event.target.closest('.navbar-toggler')) {
+        return;
+    }
+    else {
+        navbarTogglerButton.classList.remove('navbar-toggler--active');
+        navListToggler.classList.remove('nav__list--active');
+    }
+})
+
