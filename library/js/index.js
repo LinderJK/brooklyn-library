f470ebae8937713bc25a9114fcea3a93a6bd9dd0
ilya slepchenkov <slepchenkovia@gmail.com>
2023-08-29 21:09:58 +0300

    refactor: change structure index.js

--- library/js/index.js
+++ library/js/index.js
@@ -1,161 +1,127 @@
-// const tasks = [{
-//     number: 1,
-//     maxscore: 26,
-//     myscore: 26,
-//     description: 'Вёрстка соответствует макету. Ширина экрана 768px'
-// }, {
-//     number: 2,
-//     maxscore: 12,
-//     myscore: 12,
-//     description: 'Ни на одном из разрешений до 640px включительно не появляется горизонтальная полоса прокрутки'
-// }, {
-//     number: 3,
-//     maxscore: 12,
-//     myscore: 12,
-//     description: 'На ширине экрана 768рх реализовано адаптивное меню +12'
-// }, ]
+const users = userList();
+console.log(users);
 
-// function grade() {
-//     let scoreSumm = 0;
-//     for (let task of tasks) {
-//         console.log(`№${task.number} - ${task.myscore}/${task.maxscore} - ${task.description}`);
-//         scoreSumm = scoreSumm + task.myscore;
-//     }
-//     return (
-//         console.log('Максимально возможные баллы - 50'),
-//         console.log('Баллы самопроверки - ' + scoreSumm));
-// }
-// grade();
+//забираем даннные из локал
+function userList() {
+  return JSON.parse(localStorage.getItem('users')) || [];
+}
 
-
-// Через класс?
-//   class User {
-//     constructor(firstName, lastName, email, password, id) {
-//         this.userid = id;
-//         this.userFirstName = firstName;
-//         this.userSecondName = lastName;
-//         this.userEmail = email;
-//         this.userPassword = password;
-//         this.userCardNumber = this.createCard();
-
-
-//     }
-
-//     createCard () {
-//         const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-//         let cardNumber ='';
-//         let length = 8;
-//         for (let i = 0; i < length; i++) {
-//             const randomIndex = Math.floor(Math.random() * characters.length);
-//             cardNumber += characters.charAt(randomIndex);
-//           }
-//           console.log (cardNumber);
-//           return cardNumber;
-//     }
-
-//   }
-
-// const instanceName = formData.firstName + formData.lastName;
-// users[instanceName] = new User(formData.firstName, formData.lastName, formData.email, formData.password, id);
-// users.users[instanceName];
-//   const newUser = new User(1, "Иван", "Иванов", "ivan@example.com");
-
-
-//Без класса
-
-//Генератор уникального номера карты
+//генератор уникального номера карты
 function createCard() {
   const min = 100000000;
   const max = 999999999;
 
   let cardNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-  cardNumber = cardNumber.toString(16);
-  
+  cardNumber = cardNumber.toString(16).toUpperCase();
+
   return cardNumber;
 }
 
-// Обработчик на форму регистрации
 const signupButton = document.getElementById('signup-button');
 
-signupButton.addEventListener('click', () => {
+// Обработчик на кнопку регистрации
+signupButton.addEventListener('click', (evt) => {
   const inputs = document.querySelectorAll('.form__modal input');
-  console.log (inputs);
-  const formData = {};
-  const users = JSON.parse(localStorage.getItem('users')) || [];
-  console.log (users);
-  console.log (formData);
 
-  //Проверка на валидацию
-  if (validation(inputs) === false){
+  //Вызов функции валидации
+  if (validation(inputs) === false) {
     return;
   }
 
+  const formData = {};
   inputs.forEach(input => {
     formData[input.name] = input.value.trim();
   });
+
   const userEmail = formData.email.trim().toLowerCase();
   formData.email = userEmail;
 
-  // проверка существования пользователя
-  const flag = checkUser(userEmail, users);
+  // вызов функции поиска существующего пользоватля
+  let flag = checkUser(userEmail, users);
   if (flag === true) {
-    console.log ('Пользователь не создан', flag);
+    console.log('Пользователь уже существует', flag);
+    evt.preventDefault();
     return;
   }
 
+  // для тестов можно удалить потом
   if (flag === false) {
-    console.log ('Пользователь создан', flag);
-    
-    formData.CardNumber = createCard();
-    
-    users.push(formData);
-    console.log ('после push')
-    console.log (users)
-    local(users);
-
-    //если ок закрываем регистрацию открываем логин
-    setTimeout(() => {
-      modalClose(document.querySelector('.modal--active'));
-      modalOpen(document.getElementById('login-modal'));
-    }, 1000);
-    
+    console.log('Пользователь создан', flag);
   }
+
+  //генерируем карту и добавляем в локал если все ок
+  formData.CardNumber = createCard();
+  users.push(formData);
+  local(users);
+
+
+  setTimeout(() => {
+    modalClose(document.querySelector('.modal--active'));
+    // modalOpen(document.getElementById('login-modal'));
+  }, 1000);
 
 });
 
-//запись в локал
+
+//запись в локал пользователя
 function local(users) {
   localStorage.setItem('users', JSON.stringify(users));
 }
 
-//**TODO FIX BUG
+
 // фукция проверки существования пользователя
-function checkUser(email, users) {
+function checkUser(mail, users) {
   let flag = false;
 
   users.forEach(user => {
-    if (user.email === email) {
-      
+    if (user.email === mail) {
       flag = true;
       console.log("Этот пользватель существует", flag, user.email);
 
     }
-    
+
   });
-  console.log ('функция проверки', flag)
+
   return flag;
 
 }
 
 
 // функция валидации
-function validation (inputs) {
-    let isValid = true;
-    inputs.forEach( (input)=> {
-      if (input.checkValidity() === false){
-        isValid = false;
-        console.log ('NOT Valid');
-      }
-    });
-    return isValid;
-  }
+function validation(inputs) {
+  let isValid = true;
+  inputs.forEach((input) => {
+    if (input.checkValidity() === false) {
+      isValid = false;
+      console.log('NOT Valid');
+    }
+  });
+  return isValid;
+}
+
+
+// Изменене иконки после входа
+function updateIcon() {
+
+  const svgObj = document.getElementById('user-svg').getSVGDocument();
+  console.log(svgObj);
+  const icon = document.querySelector('.auth-icon__img');
+  icon.setAttribute('src', './icons/user-icon.svg');
+
+}
+
+updateIcon()
+
+
+// check card -- Digital Library Cards
+function checkCard() {
+  const users = JSON.parse(localStorage.getItem('users')) || [];
+  // const name = document.querySelector('')
+  // const number = document.querySelector('')
+  const inputs = document.querySelectorAll('.find-card__input input');
+  const button = document.querySelectorAll('.find-card__button');
+  console.log(inputs);
+  console.log(button);
+  console.log(users);
+
+}
